54de355ca482cbf983671e2a04506640ac7e35e9
peter-keller <keller.f.peter@gmail.com>
2021-10-24 22:05:22 +0100

    feat: remove-nth-node-from-end-of-list completed

---
+++ leetcode/arrays/remove-nth-node-from-end-of-list/index.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+const removeNthFromEnd = (head, n) => {
+  let slow = head
+  let fast = head
+
+  while (fast && fast.next) {
+    slow = slow.next
+    fast = fast.next.next
+  }
+
+  return slow
+};
