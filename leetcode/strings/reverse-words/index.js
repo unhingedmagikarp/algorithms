cce1bb7ba046534e3817419b0f48b9f0e13a175a
peter-keller <keller.f.peter@gmail.com>
2021-10-24 22:01:59 +0100

    feat: reverse-words completed

---
+++ leetcode/strings/reverse-words/index.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const reverseWords = (s) => {
+  return s.split(" ").map(word => word.split("").reverse().join("")).join(" ")
+};
+
+console.log(reverseWords("Let's take LeetCode contest"))
+// Output: "s'teL ekat edoCteeL tsetnoc"
