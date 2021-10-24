52a16ef606ac0007adfec2f8d8a3dbaeaeedf136
peter-keller <keller.f.peter@gmail.com>
2021-10-24 22:04:49 +0100

    feat: middle-of-linked-list completed

---
+++ leetcode/arrays/middle-of-linked-list/index.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+function ListNode(val, next) {
+  this.val = (val === undefined ? 0 : val)
+  this.next = (next === undefined ? null : next)
+}
+
+const middleNode = (head) => {
+  let slow = head
+  let fast = head
+
+  while (fast && fast.next) {
+    slow = slow.next
+    fast = fast.next.next
+  }
+
+  return slow
+};
